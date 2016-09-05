c06fe5806a03514c2776cbf02097f2af8bb9fb7a
srg <srg.post@gmail.com>
2016-09-05 16:54:19 +0700

    initial release fix

--- public/js/client.js
+++ public/js/client.js
@@ -4,7 +4,7 @@ var app = angular.module("yaSample", ['ngRoute', 'ngCookies', 'ui.bootstrap']);
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
 	    .when('/', {
-	    	temlate : "<div>hello</div>"
+	    	template : "<div>hello</div>"
 	        //templateUrl : 'static/tmpl/notes.html',
 	        //controller  : 'NotesController'
 	    });
