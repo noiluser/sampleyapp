c69addb7e8e6244f6589a6630df500b0d86a780e
srg <srg.post@gmail.com>
2016-09-06 13:36:38 +0700

    fix param name

--- public/js/factories/UserFactory.js
+++ public/js/factories/UserFactory.js
@@ -20,7 +20,7 @@ app.factory('User', function($http) {
 	// private
 	userPrivate.getUserData = function(cb) {
 		var params = {
-				url : "https://login.yandex.ru",
+				hostname : "login.yandex.ru",
 				path : "/info",
 				method : "POST",
 				params : {
