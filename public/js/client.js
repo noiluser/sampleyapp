e78c33416b471016b5ed65fc80e664267c190726
srg <srg.post@gmail.com>
2016-09-06 17:40:05 +0700

    move to the /disk

--- public/js/client.js
+++ public/js/client.js
@@ -4,6 +4,19 @@ var app = angular.module("yaSample", ['ngRoute', 'ngCookies', 'ui.bootstrap']);
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
 	    .when('/', {
+	        /*templateUrl : 'static/tmpl/explorer.html',
+	        controller  : 'ExplorerController'*/
+	    	template : "<div class=\"container\"> 							\
+							<div class=\"row\" ng-show=\"!isUserLoggedIn\"> \
+								<div class=\"col-xs-12\">					\
+									<div class=\"text-center\">				\
+										Please log in first					\
+									</div>									\
+								</div>										\
+							</div>											\
+						</div>"
+	    })
+	    .when('/disk', {
 	        templateUrl : 'static/tmpl/explorer.html',
 	        controller  : 'ExplorerController'
 	    })
