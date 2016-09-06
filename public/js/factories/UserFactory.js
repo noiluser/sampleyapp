f0df44eaf7e765ac736fe3daaa8608760af6cd4e
srg <srg.post@gmail.com>
2016-09-06 12:16:47 +0700

    fix4

--- public/js/factories/UserFactory.js
+++ public/js/factories/UserFactory.js
@@ -20,7 +20,7 @@ app.factory('User', function($http) {
 	// private
 	userPrivate.getUserData = function(cb) {
 		var url = "https://login.yandex.ru/info?callback=JSON_CALLBACK&onload=JSON_CALLBACK&oauth_token=" + this.access_token;
-		$http.post(url).
+		$http.jsonp(url).
 		    success(function(data, status, headers, config) {
 		    	console.log(data);
 		    	/*self.firstName = data.response[0].first_name;
