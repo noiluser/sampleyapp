6ec7021bb52f7057b87d63a9931df580d5ffe085
srg <srg.post@gmail.com>
2016-09-06 17:42:23 +0700

    get to the disk

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.get("/", function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get("/disk", function(req, res){
+	res.sendFile(__dirname + '/public/index.html');
+});
+
 //
 app.post('/request', requester.request);
 app.post('/login', requester.login);
