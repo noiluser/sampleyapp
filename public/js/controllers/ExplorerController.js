dab0daaff09e5a6a80dd6344fcda1a39ea0ef36b
srg <srg.post@gmail.com>
2016-09-07 14:30:07 +0700

    request files

--- public/js/controllers/ExplorerController.js
+++ public/js/controllers/ExplorerController.js
@@ -1,13 +1,20 @@
 app.controller("ExplorerController", function($scope, $routeParams, User) {
 	"ngInject"
 	$scope.isUserLoggedIn = false;
-	$scope.path = $routeParams.path;
+	$scope.path = "/" + $routeParams.path;
 	
 	$scope.$watch(function(){ return User.isAuthorized(); }, function(val){
 		if (val) {
 			$scope.isUserLoggedIn = true;
+			$scope.$emit("loadFolder");
 		} else {
 			$scope.isUserLoggedIn = false;
 		}
+	});
+	
+	$scope.$on('loadFolder', function(event) {
+		User.getContent($scope.path, function(content) {
+			
+		});
 	});
 });
